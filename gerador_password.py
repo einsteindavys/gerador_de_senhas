8037746e3d83c0c6302e527007df7efc2da496e5
Einstein Davys <95243864+einsteindavys@users.noreply.github.com>
2021-11-29 19:55:25 -0300

    Update gerador_password.py

--- gerador_password.py
+++ gerador_password.py
@@ -1,5 +1,5 @@
 import random
-import PySimpleGUI as sg
+import PySimpleGUI as tema
 import os
 
 
@@ -8,19 +8,19 @@ class GerarSenha:
 # Layout do popup
         sg.theme('Default')
         layout = [
-            [sg.Text('Site', size=(10, 1)),sg.Input(key='site', size=(20, 1))],
-            [sg.Text('Login', size=(10, 1)),sg.Input(key='login', size=(20, 1))],
-            [sg.Text('Qual o Tamanho da senha:'), sg.Combo(values=list(range(31)), key='total_chars', default_value=6, size=(3, 1))],
-            [sg.Output(size=(32, 5))],
-            [sg.Button('Gerar Senha')]
+            [tema.Text('Site', size=(10, 1)),tema.Input(key='site', size=(20, 1))],
+            [tema.Text('Login', size=(10, 1)),tema.Input(key='login', size=(20, 1))],
+            [tema.Text('Qual o Tamanho da senha:'), tema.Combo(values=list(range(31)), key='total_chars', default_value=6, size=(3, 1))],
+            [tema.Output(size=(32, 5))],
+            [tema.Button('Gerar Senha')]
         ]
 # Atributos do popup
-        self.janela = sg.Window('Gerador de Senhas', layout)
+        self.janela = tema.Window('Gerador de Senhas', layout)
 
     def Iniciar(self):
         while True:
             evento, valores = self.janela.read()
-            if evento == sg.WINDOW_CLOSED:
+            if evento == tema.WINDOW_CLOSED:
                 break
             if evento == 'Gerar Senha':
                 nova_senha = self.gerar_senha(valores)
