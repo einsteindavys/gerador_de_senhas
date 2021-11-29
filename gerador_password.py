e67da3355ac627dd7b583fa05b90fbffe54e4f85
Einstein Davys <95243864+einsteindavys@users.noreply.github.com>
2021-11-29 16:38:42 -0300

    Update gerador_password.py

--- gerador_password.py
+++ gerador_password.py
@@ -5,7 +5,7 @@ import os
 
 class GerarSenha:
     def __init__(self):
-
+# Layout do popup
         sg.theme('Default')
         layout = [
             [sg.Text('Site', size=(10, 1)),sg.Input(key='site', size=(20, 1))],
@@ -14,7 +14,7 @@ class GerarSenha:
             [sg.Output(size=(32, 5))],
             [sg.Button('Gerar Senha')]
         ]
-
+# Atributos do popup
         self.janela = sg.Window('Gerador de Senhas', layout)
 
     def Iniciar(self):
@@ -28,7 +28,7 @@ class GerarSenha:
                 self.salvar_senha(nova_senha, valores)
 
     def gerar_senha(self, valores):
-        char_list = 'qwertyuiopasdfghjklçzxcvbnmQWERTYUIOPASDFGHJKLÇZXCVBNM!@#$%¨&*()_+-=[],.;<>:^}{'
+        char_list = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%¨&*()_+-=[],.;<>:^}{'
         chars = random.choices(char_list, k=int(valores['total_chars']))
         new_pass = ''.join(chars)
         return new_pass
@@ -36,11 +36,10 @@ class GerarSenha:
     def salvar_senha(self, nova_senha, valores):
         with open('senhas.txt', 'a', newline='') as arquivo:
             arquivo.write(
-                f"site: {valores['site']}, login: {valores['login']}, nova senha: {nova_senha}")
+                f"\nsite: {valores['site']}, login: {valores['login']}, nova senha: {nova_senha}")
 
         print('Arquivo salvo')
 
 
 gerador = GerarSenha()
 gerador.Iniciar()
-
