4bf6e0cd69c56bb7a4d1616370d1005326461e8e
Einstein Davys <95243864+einsteindavys@users.noreply.github.com>
2021-11-29 15:18:00 -0300

    Add files via upload

---
+++ gerador_password.py
@@ -0,0 +1,45 @@
+import random
+import PySimpleGUI as sg
+import os
+
+
+class PassGen:
+    def __init__(self):
+# Layout do popup
+        sg.theme('Default')
+        layout = [
+            [sg.Text('Site', size=(10, 1)),sg.Input(key='site', size=(20, 1))],
+            [sg.Text('Login', size=(10, 1)),sg.Input(key='login', size=(20, 1))],
+            [sg.Text('Qual o Tamanho da senha:'), sg.Combo(values=list(range(31)), key='total_chars', default_value=6, size=(3, 1))],
+            [sg.Output(size=(32, 5))],
+            [sg.Button('Gerar Senha')]
+        ]
+# Atributos do popup
+        self.janela = sg.Window('Gerador de Senhas', layout)
+
+    def Iniciar(self):
+        while True:
+            evento, valores = self.janela.read()
+            if evento == sg.WINDOW_CLOSED:
+                break
+            if evento == 'Gerar Senha':
+                nova_senha = self.gerar_senha(valores)
+                print(nova_senha)
+                self.salvar_senha(nova_senha, valores)
+
+    def gerar_senha(self, valores):
+        char_list = 'qwertyuiopasdfghjklçzxcvbnmQWERTYUIOPASDFGHJKLÇZXCVBNM!@#$%¨&*()_+-=[],.;<>:^}{'
+        chars = random.choices(char_list, k=int(valores['total_chars']))
+        new_pass = ''.join(chars)
+        return new_pass
+
+    def salvar_senha(self, nova_senha, valores):
+        with open('senhas.txt', 'a', newline='') as arquivo:
+            arquivo.write(
+                f"site: {valores['site']}, login: {valores['login']}, nova senha: {nova_senha}")
+
+        print('Arquivo salvo')
+
+
+gen = PassGen()
+gen.Iniciar()
